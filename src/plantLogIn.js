b9237b51c35d6e8e306e18aa836657f815da94a3
artabeca <129829484+artabeca@users.noreply.github.com>
2023-04-10 10:28:20 +0300

    Create plantLogIn.js

---
+++ src/plantLogIn.js
@@ -0,0 +1,70 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import fullLogo from '../images/fullLogo.png';
+import '../plantDoctor.css';
+
+export default function SignIn() {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get("email"),
+            password: data.get("password"),
+        });
+    };
+
+    return (
+            <Container component="main" maxWidth="xs">
+                <img src={fullLogo} alt="Logo" className="logo"/>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            style={{
+                                backgroundColor:"#4f6059"
+                            }}
+                        >
+                            Log In
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+    );
+}
