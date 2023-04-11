cdb8635bde321012afc965f81027de2a09f1851e
mar-rin <125647550+mar-rin@users.noreply.github.com>
2023-04-11 15:50:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
@@ -9,4 +12,3 @@ root.render(
       </BrowserRouter>
   </React.StrictMode>
 );
-
