55338b60271db045669947dc25a6c056f1dbe594
artabeca <129829484+artabeca@users.noreply.github.com>
2023-04-12 17:20:36 +0300

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,10 @@
 import React from "react";
-import NavBar from "./navBar";
 import CalendarMain from "./calendarMain";
 import AddEvent from "./addEvent";
 
 export default function Home(){
     return(
         <div>
-            <NavBar/>
             <CalendarMain />
             <AddEvent/>
         </div>
