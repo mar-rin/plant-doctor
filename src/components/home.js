667ba5f258f429f5ccc66f60fdc14154f4a97045
artabeca <arta.beca@gmail.com>
2023-04-13 14:06:17 +0300

    added comment to home

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,9 @@ import React from "react";
 import CalendarMain from "./calendarMain";
 import AddEvent from "./addEvent";
 
+
+//Artas home file
+
 export default function Home(){
     return(
         <div>
